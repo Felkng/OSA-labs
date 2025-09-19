7429859fc43aa0467ef044402338cc6718dd4e9f
felkng <felipersf374@gmail.com>
2025-09-19 16:36:16 -0300

    Remove unnecessary code in Buffer class

--- lab02/Buffer.cpp
+++ lab02/Buffer.cpp
@@ -3,23 +3,21 @@
 
 using namespace std;
 
-void write_fixed_registro(Registro reg, ofstream out) {
-  if (!out.is_open()) {
-    cerr << "File is not open.";
-    return;
-  }
+void write_fixed_registro(Registro reg, ofstream &out) {
   char *buffer = reg.pack_fixed();
   out.write(buffer, PACKET_SIZE);
 }
 
-Registro Buffer::read_fixed_registro(ifstream in) {
+Registro Buffer::read_fixed_registro(ifstream &in) {
   if (!in.is_open()) {
     cerr << "File is not open.";
     return Registro();
   }
   char *buffer;
-  in.read(buffer, PACKET_SIZE);
-  Registro reg;
-  reg.unpack_fixed(buffer);
-  return reg;
+  if (in.read(buffer, PACKET_SIZE)) {
+    Registro reg;
+    reg.unpack_fixed(buffer);
+    return reg;
+  }
+  return Registro();
 }
--- lab02/Buffer.h
+++ lab02/Buffer.h
@@ -9,8 +9,8 @@ using namespace std;
 
 class Buffer {
 public:
-  void write_fixed_registro(Registro reg, ofstream out);
-  Registro read_fixed_registro(ifstream in);
+  void write_fixed_registro(Registro reg, ofstream &out);
+  Registro read_fixed_registro(ifstream &in);
 };
 
 #endif
