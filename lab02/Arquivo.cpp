01ca11009c1a760b9b14d109210484e3ca298c57
felkng <felipersf374@gmail.com>
2025-09-22 20:49:47 -0300

    Add read and save fixed registers in binary
    What was added:
    - Function to read csv files
    - Write and Read binary files

--- lab02/Arquivo.cpp
+++ lab02/Arquivo.cpp
@@ -1,13 +1,15 @@
 #include "Arquivo.h"
 #include "Buffer.h"
 #include <fstream>
+#include <string>
 #include <vector>
 using namespace std;
 
 Arquivo::Arquivo(string filename) : filename(filename) {}
-void Arquivo::add_fixed_registro(Registro reg) {
+
+void Arquivo::add_fixed_registro(Registro &reg) {
   Buffer buffer;
-  ofstream file(this->filename, ios::app);
+  ofstream file(this->filename, ios::app | ios::binary);
   if (!file.is_open()) {
     cerr << "File is not open.";
     return;
@@ -18,7 +20,7 @@ void Arquivo::add_fixed_registro(Registro reg) {
 
 vector<Registro> Arquivo::read_fixed_registros() {
   Buffer buffer;
-  ifstream file(this->filename, ios::in);
+  ifstream file(this->filename, ios::in | ios::binary);
   if (!file.is_open()) {
     cerr << "File is not open.";
     return {};
@@ -27,6 +29,31 @@ vector<Registro> Arquivo::read_fixed_registros() {
   vector<Registro> registros;
   while (true) {
     Registro reg = buffer.read_fixed_registro(file);
+    if (reg.is_empty()) {
+      break;
+    }
+    registros.push_back(reg);
+  }
+  return registros;
+}
+
+vector<Registro> Arquivo::read_csv_file(char sep, int skiprows) {
+  Buffer buffer;
+  ifstream file(this->filename, ios::in);
+  if (!file.is_open()) {
+    cerr << "File is not open.";
+    return {};
+  }
+
+  while (skiprows > 0) {
+    string line;
+    getline(file, line);
+    skiprows--;
+  }
+
+  vector<Registro> registros;
+  while (true) {
+    Registro reg = buffer.read_line_csv(file, sep);
     if (reg.is_empty()) {
       break;
     }
