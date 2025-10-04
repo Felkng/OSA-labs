c9667a89a8c53a2574f98dbecd9b6466b60c02da
felkng <felipersf374@gmail.com>
2025-10-04 13:12:24 -0300

    Add HEADER_SIZE constant expression

--- lab03/constantes.h
+++ lab03/constantes.h
@@ -1,8 +1,11 @@
 #ifndef CONSTANTES_H
 #define CONSTANTES_H
 
+#include "FileHeader.h"
 #define NOME_FIXO 50
 #define MATRICULA_FIXO 20
 #define CURSO_FIXO 10
+
+constexpr size_t HEADER_SIZE = sizeof(FileHeader);
 
 #endif
