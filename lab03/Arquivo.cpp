ec39e835560e109956e58aed0afd85b8dbd045f9
felkng <felipersf374@gmail.com>
2025-10-05 18:26:54 -0300

    Fix polymorphism and file operations

--- lab03/Arquivo.cpp
+++ lab03/Arquivo.cpp
@@ -5,6 +5,7 @@
 #include "RegistroAluno.h"
 #include "constantes.h"
 #include <fstream>
+#include <memory>
 using namespace std;
 
 template <>
@@ -14,13 +15,27 @@ Arquivo<Registro>::Arquivo(string filename, Formato formato)
 template <> void Arquivo<Registro>::adicionar_registro(Registro &reg) {
   fstream file(this->filename, ios::in | ios::out | ios::binary);
 
+  bool is_new_file = false;
+  
   if (!file.is_open()) {
-    cerr << "Couldn't open the file \"" << this->filename << "\"." << endl;
-    return;
+    // Tenta criar o arquivo se não existir
+    file.open(this->filename, ios::out | ios::binary);
+    if (!file.is_open()) {
+      cerr << "Não foi possível abrir o arquivo: \"" << this->filename << "\"."
+           << endl;
+      return;
+    }
+    is_new_file = true;
+    file.close();
+    file.open(this->filename, ios::in | ios::out | ios::binary);
   }
 
   FileHeader header;
-  bool header_read_success = read_header(file, header);
+  bool header_read_success = false;
+  
+  if (!is_new_file) {
+    header_read_success = read_header(file, header);
+  }
 
   if (!header_read_success) {
     // inicializa novo cabeçalho
@@ -35,62 +50,99 @@ template <> void Arquivo<Registro>::adicionar_registro(Registro &reg) {
   reg.pack(buffer, this->formato);
   long long record_size = buffer.data.size();
   buffer.write(file);
+  file.flush();
 
   header.num_registros++;
   header.total_data_size += record_size;
   header.last_modified = time(nullptr);
 
-  if (!write_header(file, header)) {
-    cerr << "Couldn't update the header file." << endl;
-  }
+
+  file.seekp(0, ios::beg);
+  file.write(reinterpret_cast<const char *>(&header), HEADER_SIZE);
+  file.flush();
+  
   file.close();
 }
 
-template <> vector<Registro> Arquivo<Registro>::ler_registros() {
+template <> vector<unique_ptr<Registro>> Arquivo<Registro>::ler_registros() {
   fstream file(this->filename, ios::in | ios::binary);
-  vector<Registro> registros;
+  vector<unique_ptr<Registro>> registros;
 
   if (!file.is_open()) {
-    cerr << "Couldn't open the file \"" << this->filename << "\"." << endl;
+    cerr << "Não foi possível abrir o arquivo: \"" << this->filename << "\"."
+         << endl;
     return registros;
   }
 
   FileHeader header;
   if (!read_header(file, header)) {
-    cerr << "Couldn't read the header." << endl;
+    cerr << "Não foi possível ler o cabeçalho do arquivo." << endl;
+    file.close();
     return registros;
   }
 
   Buffer buffer;
   file.seekg(HEADER_SIZE, ios::beg);
   if (!buffer.read(file, header.total_data_size)) {
-    cerr << "Couldn't read file data." << endl;
+    cerr << "Não foi possível ler o conteúdo do arquivo." << endl;
     return registros;
   };
 
+  buffer.pointer = 0;
+
   for (int i = 0; i < header.num_registros; ++i) {
     // PRECISA ESPECIALIZAR PARA PODER UTILIZAR MÉTODOS DE PACK E UNPACK
-    RegistroAluno reg_aluno;
-    reg_aluno.unpack(buffer, this->formato);
-    registros.push_back(reg_aluno);
+    unique_ptr<RegistroAluno> reg_aluno = make_unique<RegistroAluno>();
+    reg_aluno->unpack(buffer, this->formato);
+    registros.push_back(std::move(reg_aluno));
   }
+  file.close();
   return registros;
 }
 
-template <class T> vector<T> Arquivo<T>::read_csv_file(char sep, int skiprows) {
-  return vector<T>();
+template <>
+vector<unique_ptr<Registro>> Arquivo<Registro>::read_csv_file(char sep,
+                                                              int skiprows) {
+  Buffer buffer;
+  fstream file(this->filename, ios::in);
+  if (!file.is_open()) {
+    cerr << "Não foi possível abrir o arquivo: \"" << this->filename << "\"."
+         << endl;
+    return {};
+  }
+
+  string line;
+  while (skiprows > 0) {
+    getline(file, line);
+    skiprows--;
+  }
+
+  vector<unique_ptr<Registro>> registros;
+  while (getline(file, line)) {
+    Buffer buffer;
+    unique_ptr<RegistroAluno> reg = make_unique<RegistroAluno>();
+    *reg = reg->parse_csv(line, sep);
+    registros.push_back(std::move(reg));
+  }
+  file.close();
+  return registros;
 }
 
 template <class T>
 bool Arquivo<T>::read_header(std::fstream &file, FileHeader &header) {
   file.seekg(0, std::ios::beg);
+  if (file.fail()) return false;
+  
   file.read(reinterpret_cast<char *>(&header), HEADER_SIZE);
-  return file.gcount() == HEADER_SIZE;
+  return file.gcount() == HEADER_SIZE && file.good();
 }
 
 template <class T>
 bool Arquivo<T>::write_header(std::fstream &file, const FileHeader &header) {
   file.seekp(0, std::ios::beg);
+  if (file.fail()) return false;
+  
   file.write(reinterpret_cast<const char *>(&header), HEADER_SIZE);
+  file.flush();
   return file.good();
 }
--- lab03/Arquivo.h
+++ lab03/Arquivo.h
@@ -1,8 +1,8 @@
 #ifndef ARQUIVO_H
 #define ARQUIVO_H
 #include "FileHeader.h"
-#include "Registro.h"
 #include "formato.h"
+#include <memory>
 #include <string>
 #include <vector>
 
@@ -15,8 +15,8 @@ public:
 
   Arquivo(string filename, Formato formato);
   void adicionar_registro(T &reg);
-  vector<T> ler_registros();
-  vector<T> read_csv_file(char sep, int skiprows = 1);
+  vector<unique_ptr<T>> ler_registros();
+  vector<unique_ptr<T>> read_csv_file(char sep, int skiprows = 1);
   bool read_header(fstream &file, FileHeader &header);
   bool write_header(fstream &file, const FileHeader &header);
 };
